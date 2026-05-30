1ab29f5b2eadbcfd00b95eefce2ed77b3dc8ece9
nirdesh-timilsina <080bct049.nirdesh@pcampus.edu.np>
2026-05-30 17:52:13 +0545

    Users can now select semesters to navigate on required subjects

--- questions/urls.py
+++ questions/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('semester/<int:semester>/',views.semester_detail,name='semester_detail'),
     path('subject/<str:subject_code>/', views.subject_detail, name='subject_detail'),
 ]
--- questions/views.py
+++ questions/views.py
@@ -5,14 +5,28 @@ from .models import Subject,Chapter,Question
 
 def home(request):
     subjects = Subject.objects.all().order_by('semester', 'code')
-    return render(request, 'questions/home.html', {'subjects': subjects})
 
+    semesters = {}
+    for subject in subjects:
+        sem = subject.semester
+        if sem not in semesters:
+            semesters[sem]= []
+        semesters[sem].append(subject)
+    return render(request, 'questions/home.html', {'semesters': semesters})
 def subject_detail(request, subject_code):
     subject = get_object_or_404(Subject, code=subject_code)
     chapters = subject.chapters.prefetch_related(
         'questions__appearances'
     ).order_by('order')
     return render(request, 'questions/subject.html', {
+        
         'subject': subject,
         'chapters': chapters,
+    })
+
+def semester_detail(request, semester):
+    subjects = Subject.objects.filter(semester=semester).order_by('code')
+    return render(request, 'questions/semester.html', {
+        'semester': semester,
+        'subjects': subjects,
     })
